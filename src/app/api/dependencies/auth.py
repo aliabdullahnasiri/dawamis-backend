47f2b1578f65e34f526dc2f576cc10e7b91fac64
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-09-05 15:38:02 +0430

    feat(dependencies/auth.py): add CurrentUserCan type

--- src/app/api/dependencies/auth.py
+++ src/app/api/dependencies/auth.py
@@ -1,13 +1,16 @@
-from typing import Annotated, Any
+from typing import Annotated, Any, Generic, Type, TypeVar
+from uuid import UUID
 
 from fastapi import Depends
 
 from app.core.security import (
+    current_user_can,
     get_current_claims,
     get_current_refresh_token,
     get_current_token,
     get_current_user_uuid,
 )
+from app.models.permission import Permission
 
 CurrentToken = Annotated[
     str,
@@ -25,6 +28,14 @@ CurrentClaims = Annotated[
 ]
 
 CurrentUserUUID = Annotated[
-    str,
+    UUID,
     Depends(get_current_user_uuid),
 ]
+
+
+class CurrentUserCan:
+    def __class_getitem__(cls, permissions: str | tuple) -> Annotated:
+        if isinstance(permissions, str):
+            permissions = (permissions,)
+
+        return Annotated[None, Depends(current_user_can(*permissions))]
