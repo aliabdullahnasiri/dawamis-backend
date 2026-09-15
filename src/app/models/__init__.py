6016f5eabcfc47c0cc383fd26890a809de302b39
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-09-15 07:25:08 +0430

    feat: import new models

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,5 +1,7 @@
 from app.models.associations import RolePermission, UserRole
 from app.models.base import Base
+from app.models.branch import Branch
+from app.models.organization import Organization
 from app.models.permission import Permission
 from app.models.role import Role
 from app.models.user import User
@@ -11,4 +13,6 @@ __all__ = [
     "Permission",
     "UserRole",
     "RolePermission",
+    "Organization",
+    "Branch",
 ]
