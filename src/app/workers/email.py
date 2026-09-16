e9990aeffa384aa95b5c80bcca0203fc411cf71b
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-09-16 17:07:16 +0430

    feat(workers/email.py): add email worker

---
+++ src/app/workers/email.py
@@ -0,0 +1,22 @@
+import asyncio
+from typing import Any, Self
+
+from app.types import AsyncService
+from app.workers.base import BaseWorker
+
+
+class EmailWorker(BaseWorker):
+    name = "email.send"
+
+    @staticmethod
+    def send(
+        service: AsyncService,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        asyncio.run(
+            service(
+                *args,
+                **kwargs,
+            )
+        )
