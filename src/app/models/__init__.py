c8c9f41c1576bec7425266d6c4852261616bc02a
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-09-25 18:15:37 +0430

    feat(models): export UserSession from models package

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,18 +1,22 @@
 from app.models.associations import RolePermission, UserRole
+from app.models.associations.organization_user import OrganizationUser
 from app.models.base import Base
 from app.models.branch import Branch
 from app.models.organization import Organization
 from app.models.permission import Permission
 from app.models.role import Role
 from app.models.user import User
+from app.models.user_session import UserSession
 
 __all__ = [
     "Base",
     "Branch",
     "Organization",
+    "OrganizationUser",
     "Permission",
     "Role",
     "RolePermission",
     "User",
     "UserRole",
+    "UserSession",
 ]
